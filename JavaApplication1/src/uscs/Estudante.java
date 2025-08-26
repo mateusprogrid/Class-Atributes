fc58f47265aa0e0a9888b74a0bd9fa2803673701
mateusmelo777 <mateusmelo.sm01@gmail.com>
2025-08-26 11:12:01 -0300

    criando classe

---
+++ JavaApplication1/src/uscs/Estudante.java
@@ -0,0 +1,12 @@
+package uscs;
+
+public class Estudante {
+    
+    String nome;
+    int idade;
+    
+    public void escrevenome(String nome) {
+        System.out.println("nome");
+    }
+    
+}
---
+++ JavaApplication1/src/uscs/TestaEstudante.java
@@ -0,0 +1,16 @@
+package uscs;
+
+public class TestaEstudante {
+
+    public static void main(String[] args) {
+        
+        Estudante A1 = new Estudante();
+        
+        A1.nome = "Mateus Severino de Melo";
+        A1.idade = 22;
+        
+        A1.escrevenome("Teodoro Samapaio da Silva");
+                  
+    }
+    
+}
