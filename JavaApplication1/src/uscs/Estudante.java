3f81b756477f4e7af23cf64c86f3f5a3b3818429
mateusmelo777 <mateusmelo.sm01@gmail.com>
2025-08-27 10:35:56 -0300

    new class

--- JavaApplication1/src/uscs/Estudante.java
+++ JavaApplication1/src/uscs/Estudante.java
@@ -1,12 +1,20 @@
 package uscs;
-
 public class Estudante {
-    
     String nome;
     int idade;
-    
-    public void escrevenome(String nome) {
-        System.out.println("nome");
+    double nota;
+    String materia;
+    //Métodos
+    public void estapresente() {
+        System.out.println(nome + " [Aluno(a) Presente!!]");
     }
-    
+    public void faltounaaula() {
+        System.out.println(nome + " [Aluno(a) Faltou!!]");
+    }
+    public void imprimir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Nota: " + nota);
+        System.out.println("Matéria: " + materia);
+    }
 }
--- JavaApplication1/src/uscs/TestaEstudante.java
+++ JavaApplication1/src/uscs/TestaEstudante.java
@@ -1,15 +1,20 @@
 package uscs;
-
 public class TestaEstudante {
-
     public static void main(String[] args) {
-        
-        Estudante A1 = new Estudante();
-        
-        A1.nome = "Mateus Severino de Melo";
-        A1.idade = 22;
-        
-        A1.escrevenome("Teodoro Samapaio da Silva");
+        Estudante Aluno1 = new Estudante();
+        Estudante Aluno2 = new Estudante();
+        Aluno1.nome = "Gabriel Carlos da Silva";
+        Aluno1.idade = 22;
+        Aluno1.nota = 9.28;
+        Aluno1.materia = "Lógica de Programação";
+        Aluno1.imprimir();
+        Aluno1.estapresente();
+        Aluno2.nome = "Beatriz Pereira de Melo";
+        Aluno2.idade = 19;
+        Aluno2.nota = 8.75;
+        Aluno2.materia = "Lógica de Programação";
+        Aluno2.imprimir();
+        Aluno2.faltounaaula();
                   
     }
     
